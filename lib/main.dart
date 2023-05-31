0f12b766a22abdc094532782cf4691ee61e00246
shmzzzz <slt0623@gmail.com>
2023-06-01 07:32:17 +0900

    リファクタ

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:adv_basics/center_column.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.purple[900],
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                'assets/images/quiz-logo.png',
-                width: 300,
-              ),
-              const SizedBox(
-                height: 100,
-              ),
-              const Text(
-                'Learn Flutter the fun way!',
-                style: TextStyle(
-                    color: Color.fromARGB(231, 255, 255, 255), fontSize: 20),
-              ),
-              const SizedBox(
-                height: 0,
-              ),
-              OutlinedButton(
-                onPressed: () {},
-                style: OutlinedButton.styleFrom(
-                  foregroundColor: const Color.fromARGB(231, 255, 255, 255),
-                  textStyle: const TextStyle(
-                    fontSize: 15,
-                  ),
-                ),
-                child: const Text(
-                  'Start Quiz',
-                ),
-              ),
-            ],
-          ),
-        ),
+        body: const CenterColumn(),
       ),
     ),
   );
