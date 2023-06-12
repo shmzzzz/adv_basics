004cb810657fab87a963c7614b692f9220277aa9
shmzzzz <slt0623@gmail.com>
2023-06-12 21:44:40 +0900

    ボタンのカスタムウィジェット化

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,4 @@
-import 'dart:ffi';
-
+import 'package:adv_basics/answer_button.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -19,27 +18,26 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('The question..'),
+          const Text(
+            'The question..',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
           const SizedBox(
             height: 30,
           ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text(
-              'Answer 1',
-            ),
+          AnswerButton(
+            answerText: 'Answer 1',
+            onTap: () {},
           ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text(
-              'Answer 2',
-            ),
+          AnswerButton(
+            answerText: 'Answer 2',
+            onTap: () {},
           ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text(
-              'Answer 3',
-            ),
+          AnswerButton(
+            answerText: 'Answer 3',
+            onTap: () {},
           ),
         ],
       ),
