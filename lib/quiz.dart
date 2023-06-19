7bbf4540467b7478d4968410e1dc07547d0654a8
shmzzzz <slt0623@gmail.com>
2023-06-19 14:50:45 +0900

    回答完了時のクラッシュを回避するために、一時的に最初の画面に遷移するように修正

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:adv_basics/start_screen.dart';
 import 'package:adv_basics/questions_screen.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -13,7 +14,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  final List<String> selectedAnswers = [];
+  List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   void switchScreen() {
@@ -21,9 +22,17 @@ class _QuizState extends State<Quiz> {
       activeScreen = 'questions-screen';
     });
   }
- 
+
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      // 結果画面に遷移させたいが、現状はないので一旦start_screenに遷移するようにする
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'start-screen';
+      });
+    }
   }
 
   @override
