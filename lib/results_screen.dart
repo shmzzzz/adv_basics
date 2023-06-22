4ff7b16a27874f4fc7c45ce63216739cddd271cd
shmzzzz <slt0623@gmail.com>
2023-06-22 21:30:54 +0900

    結果画面に表示するための情報をまとめるgetSummaryDataメソッドの追加

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:adv_basics/data/questions.dart";
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({
@@ -7,6 +8,23 @@ class ResultsScreen extends StatelessWidget {
   });
 
   final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'questions_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i]
+        },
+      );
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
