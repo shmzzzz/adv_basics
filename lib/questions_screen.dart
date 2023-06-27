84164e6173e653f626ef8b4898cce60f502792af
shmzzzz <slt0623@gmail.com>
2023-06-27 09:29:01 +0900

    リファクタ(private, getter, アロー関数)

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -49,7 +49,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             const SizedBox(
               height: 30,
             ),
-            ...currentQuestion.getShuffledAnswers().map(
+            ...currentQuestion.shuffledAnswers.map(
               (answer) {
                 return AnswerButton(
                   answerText: answer,
